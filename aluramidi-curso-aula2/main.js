02d578c84dd720d633e718da74a0b8cde0da671c
fernanda1417 <135229119+fernanda1417@users.noreply.github.com>
2023-10-23 10:06:29 -0300

    ;;

--- aluramidi-curso-aula2/main.js
+++ aluramidi-curso-aula2/main.js
@@ -4,8 +4,15 @@ function tocaSom(idElementoAudio) {
 const listadeTeclas=document.querySelectorAll('tecla');
 let contador=0;
 
-while(contador<listadeTeclas.length){
-listadeTeclas[contador].onclick= function () {};
+while(contador<listadeTeclas.length){}
+const tecla= listadeTeclas[contador]
+
+const instrumento=tecla.classList[1]
+const idAudio=`#som_$(instrumento)`;
+tecla.onclick=function(){
+    tocaSom(idAudio)
+}
+tecla.onclick= function () {};
 contador=contador+1;
 }
 
